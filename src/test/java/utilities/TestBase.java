aa2d5df553e94633a6f2e5df13cf3485b9e70a7d
begmyratg <begmyratg@gmail.com>
2022-11-29 09:55:05 -0600

    remove testng soft assert import

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -7,7 +7,6 @@ import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
 
